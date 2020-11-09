c50899cad142874cc0fce51376cf8191bdc41440
OskarFranck <70700002+OskarFranck@users.noreply.github.com>
2020-11-09 13:15:58 +0100

    Oskar test for demo

---
+++ Skeleton.java
@@ -0,0 +1,10 @@
+public abstract class Skeleton extends Monster{
+
+    public Skeleton() {
+
+    }
+
+    public Skeleton(double initiative, double health, double attack, double agility, double rarity) {
+        super(initiative, health, attack, agility, rarity);
+    }
+}
