3ccfb3d0f517652258707bedac982a5a22ff970d
OskarFranck <70700002+OskarFranck@users.noreply.github.com>
2020-11-11 13:07:45 +0100

    Oskar Test 32133

--- Skeleton.java
+++ Skeleton.java
@@ -1,9 +1,5 @@
 public class Skeleton extends Monster{
 
-    public Skeleton() {
-
-    }
-
     public Skeleton(int initiative, int health, int attack, int agility, int rarity) {
         super(initiative, health, attack, agility, rarity);
     }
