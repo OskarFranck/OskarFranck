723abcfd61fc86da30c564febc304d6b7dbf8d20
OskarFranck <oskar.franck@gmail.com>
2020-11-11 11:08:04 +0100

    Ny fightclass

--- Fighting.java
+++ Fighting.java
@@ -1,14 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Fighting {
-    static Character newPlayer = new Knight(5, 9,6,4);  //skapar en charactar med namn knight
+    static Character newPlayer = new Knight(5, 9,6,10);  //skapar en charactar med namn knight
     static Monster newMonster = new Skeleton(4, 2, 3, 3, 1); //skapar ett monster med namn skeleton
 
 
     public static void whoStartsFight() {
         int playerInitiative = rollDice(newPlayer.getInitiative());
-        int monsterInitiative = rollDice(newMonster.getInitiative());
+        double monsterInitiative = rollDiceDouble(newMonster.getInitiative());
 
+        while (tryToFlee(newPlayer.getAgility()))
         if (playerInitiative > monsterInitiative) {
             fightingP();
         } else {
@@ -17,22 +19,22 @@ public class Fighting {
     }
     public static void playerAttack() {
         int playerAttack = rollDice(newPlayer.getAttack());
-        int monsterDefend = rollDice(newMonster.getAgility());
+        double monsterDefend = rollDiceDouble(newMonster.getAgility());
 
         if (playerAttack > monsterDefend) {
-            newMonster.setHealth(newMonster.getHealth() -1);
+            newMonster.setDurability(newMonster.getDurability() -1);
             System.out.println("The knight swings his sword and HITS!");
             System.out.println("Monster looses one HP");
-            System.out.println("HP left: " + newMonster.getHealth());
+            System.out.println("HP left: " + newMonster.getDurability());
         } else {
             System.out.println("The knight swings his sword and MISSES!");
             System.out.println("Monster Dodges");
-            System.out.println("HP left: " + newMonster.getHealth());
+            System.out.println("HP left: " + newMonster.getDurability());
         }
     }
 
     public static void monsterAttack() {
-        int monsterAttack = rollDice(newMonster.getAttack());
+        double monsterAttack = rollDiceDouble(newMonster.getAttack());
         int playerDefend = rollDice(newPlayer.getAgility());
 
         if (monsterAttack > playerDefend) {
@@ -48,20 +50,61 @@ public class Fighting {
         do {
             monsterAttack();
             playerAttack();
-        } while (newMonster.getHealth() > 0 && newPlayer.getHealth() > 0);
+        } while (newMonster.getDurability() > 0 && newPlayer.getHealth() > 0);
     }
 
     public static void fightingP() {
         do {
             playerAttack();
             monsterAttack();
-        } while (newPlayer.getHealth() > 0 && newMonster.getHealth() > 0);
+        } while (newPlayer.getHealth() > 0 && newMonster.getDurability() > 0);
+    }
+
+    public static boolean tryToFlee(int agility) {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        int randomGen;
+        int chanceToFlee;
+        int fleeInput = 0;
+        randomGen = rand.nextInt(99) + 1;
+        chanceToFlee = agility * 10;
+
+        System.out.println("You see a monster, will you fight or try to flee?\n [1]. Fight [2]. Flee");
+
+        fleeInput = sc.nextInt();
+
+        switch (fleeInput) {
+            case 2:
+                if (randomGen <= chanceToFlee) {
+                    return false;
+                } else {
+                    return true;
+                }
+            default:
+                return true;
+        }
     }
 
 
     public static int rollDice(int numberOfDice) {
         int diceThrow;
         int diceTotalScore = 0;
+        Random rand = new Random();
+
+        for (int i = 0; i < numberOfDice; i++) {  //numberOfDice == initiativet hos karaktären / monstret
+
+            diceThrow = rand.nextInt(6) + 1;
+
+            //  System.out.println(diceThrow);
+
+            diceTotalScore = diceTotalScore + diceThrow;
+
+        }
+        return diceTotalScore;
+    }
+    public static double rollDiceDouble(double numberOfDice) {
+        double diceThrow;
+        double diceTotalScore = 0;
         Random rand = new Random();
 
         for (int i = 0; i < numberOfDice; i++) {  //numberOfDice == initiativet hos karaktären / monstret
