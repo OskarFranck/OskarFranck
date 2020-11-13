d07074c6acef69dbc035d838bacf9cf5e0a4360c
HA-ANgit <hannes93andersson@gmail.com>
2020-11-13 14:20:11 +0100

    Music and soundFx

--- Room.java
+++ Room.java
@@ -2,7 +2,7 @@ public class Room {
     public static void enterRoom(){
         System.out.println("You enter a room"); //Addera metod som ropar på kararktären
         System.out.println("You might see a monster"); //Addera method för att generera random monster
-        battlemusic();
+        battlemusic();//Om monster dyker upp startar fight-klassen inkl SoundFx
         System.out.println("Vill du stanna och slåss? Eller fly!");
         System.out.println("Fight!");//Om stanna, Här startar fighten
         System.out.println("Om du överlever kan det finnas skatter i rummet!");
